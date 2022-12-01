089f19335bf895570ed4be611c7fb57fb1d93145
MaksimA1 <118662976+MaksimA1@users.noreply.github.com>
2022-12-01 20:33:03 +0300

    Add files via upload

---
+++ module5_homework/task2.js
@@ -0,0 +1,12 @@
+let num;
+
+if ((typeof(num) == 'number')&&(!isNaN(num))) {
+    console.log("value is Number");
+} else if (typeof(num) == 'string') {
+    console.log("value is String");
+} else if (typeof(num) == 'boolean') {
+    console.log("value is Boolean");
+} else {
+    console.log("value X is undefined");
+}
+
